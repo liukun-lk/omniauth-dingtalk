9c5e8197d735ece19ed35ef2ad0e548cbcaa61fa
feelmix <feelmix@feelmixdeMBP.lan>
2017-12-12 00:08:39 +0800

    add dingding omniauth strategies

--- lib/oauth2_dingtalk.rb
+++ lib/oauth2_dingtalk.rb
@@ -1,4 +1,5 @@
-require "oauth2_dingtalk/version"
+require_relative "oauth2_dingtalk/version"
+require_relative "omniauth/strategies/dingding"
 
 module Oauth2Dingtalk
   # Your code goes here...
---
+++ lib/omniauth/strategies/dingding.rb
@@ -0,0 +1,56 @@
+require "omniauth-oauth2"
+
+module OmniAuth
+  module Strategies
+    class Dingding < OmniAuth::Strategies::OAuth2
+      option :name, "dingding"
+
+      option :client_options, {
+        site:          "https://oapi.dingtalk.com",
+        authorize_url: "/connect/qrconnect",
+        token_url:     "/sns/gettoken",
+        persistent_url: "/sns/get_persistent_code",
+        token_method: :get
+      }
+
+      option :authorize_params, { scope: 'snsapi_login' }
+
+      uid do
+        raw_info['openid']
+      end
+
+      info do
+        {
+          unionid: raw_info['unionid']
+        }
+      end
+
+      extra do
+        { raw_info: raw_info }
+      end
+
+      def request_phase
+        params = client.auth_code.authorize_params.merge(redirect_uri: callback_url).merge(authorize_params)
+        params["appid"] = params.delete("client_id")
+        redirect client.authorize_url(params)
+      end
+
+      def raw_info
+        @raw_info ||= access_token.post(options.client_options[:persistent_url] + "?access_token=#{access_token.token}") do |req|
+          req.headers['Content-Type'] = 'application/json'
+          req.body = "{\"tmp_auth_code\":\"#{request.params['code']}\"}"
+        end.parsed
+      end
+
+      protected
+
+      def build_access_token
+        params = {
+          'appid' => client.id,
+          'appsecret' => client.secret
+          }
+        client.get_token(params)
+      end
+    end
+  end
+end
